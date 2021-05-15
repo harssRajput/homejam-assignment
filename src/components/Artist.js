f58115b11f8afc029d76955cffdd54672e12ec5b
Harsh Rajput <hrrajput20@gmail.com>
2021-05-15 12:27:42 +0530

    add artist dataset and convert artistlist into class based component

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,15 +1,16 @@
 import bookmark from "../bookmark.png";
 
 
-function Artist() {
+function Artist(props) {
+  const {artist} = props;
   return (
     <div className="artist card my-5">
-      <img src="https://wallpapercave.com/wp/wp4417868.jpg" className="card-img-top artist-image" alt="artist image" />
+      <img src={artist.image} className="card-img-top artist-image" alt="artist image" />
       <div className="card-body pb-4">
-        <h5 className="card-title">Card title</h5>
+        <h5 className="card-title">{artist.name}</h5>
       </div>
       <div className="card-body d-flex pt-0 mb-9 justify-content-between">
-        <a href="#" className="card-link more-info"> 
+        <a href={artist.moreDetail} className="card-link more-info"> 
           more info -&gt;
         </a>
           <img className="bookmark-icon" src={bookmark} alt="bookmark icon" />
--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -1,62 +1,73 @@
-import Artist from "./Artist";
+import React from "react";
 
-function ArtistList() {
-  return (
-    <div className="artist-list container">
-      <div className="row header">
-        <h2 className="col-5 text-center">Upcoming Shows</h2>
-        {/* {dfkjas;lf} */}
-        <div className="col-3 text-center offset-md-4">
-          <nav aria-label="artist navigation">
-            <ul class="pagination">
-              <li class="page-item ">
-                <a
-                  class="page-link bg-dark text-white"
-                  href="#"
-                  aria-label="Previous"
-                >
-                  <span aria-hidden="true">&laquo;</span>
-                </a>
-              </li>
-              {/* <li class="page-item">
-                <a class="page-link" href="#">
-                  1
-                </a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">
-                  2
-                </a>
-              </li> */}
-              <li class="page-item">
-                <a
-                  class="page-link bg-dark text-white"
-                  href="#"
-                  aria-label="Next"
-                >
-                  <span aria-hidden="true">&raquo;</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
+import Artist from "./Artist";
+import { artistsDataset } from "../dataset/artistsDataset";
+
+class ArtistList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artists: artistsDataset,
+    };
+  }
+
+  render() {
+    //   console.log(artistsDataset);
+    const { artists } = this.state;
+    return (
+      <div className="artist-list container">
+        <div className="row header">
+          <h2 className="col-5 text-center">Upcoming Shows</h2>
+          <div className="col-3 text-center offset-md-4">
+            <nav aria-label="artist navigation">
+              <ul class="pagination">
+                <li class="page-item ">
+                  <a
+                    class="page-link bg-dark text-white"
+                    href="#"
+                    aria-label="Previous"
+                  >
+                    <span aria-hidden="true">&laquo;</span>
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a
+                    class="page-link bg-dark text-white"
+                    href="#"
+                    aria-label="Next"
+                  >
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div className="row justify-content-evenly row-cols-1 row-cols-md-2 row-cols-lg-4">
+          {artists.map((artist) => {
+            return (
+              <div className="col">
+                <Artist artist={artist} />
+              </div>
+            );
+          })}
+          {/* <div className="col">
+            <Artist />
+          </div>
+          <div className="col">
+            <Artist />
+          </div>
+          <div className="col">
+            <Artist />
+          </div>
+          <div className="col">
+            <Artist /> 
+        </div>*/}
         </div>
       </div>
-      <div className="row justify-content-evenly row-cols-1 row-cols-md-2 row-cols-lg-4">
-        <div className="col">
-          <Artist />
-        </div>
-        <div className="col">
-          <Artist />
-        </div>
-        <div className="col">
-          <Artist />
-        </div>
-        <div className="col">
-          <Artist />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ArtistList;
