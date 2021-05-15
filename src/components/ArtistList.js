842146813904e4b21da09f5f27c53900026fac3c
Harsh Rajput <hrrajput20@gmail.com>
2021-05-15 11:41:03 +0530

    add headers in review list and artist list

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -1,25 +1,62 @@
-import Artist from "./Artist"
+import Artist from "./Artist";
 
 function ArtistList() {
-    return (
-        <div className="artist-list container">
-            <div className="row justify-content-evenly row-cols-1 row-cols-md-2 row-cols-lg-4">
-                <div className="col">
-                    <Artist/>
-                </div>
-                <div className="col">
-                    <Artist/>
-                </div>
-                <div className="col">
-                    <Artist/>
-                </div>
-                <div className="col">
-                    <Artist/>
-                </div>
-            </div>
+  return (
+    <div className="artist-list container">
+      <div className="row header">
+        <h2 className="col-5 text-center">Upcoming Shows</h2>
+        {/* {dfkjas;lf} */}
+        <div className="col-3 text-center offset-md-4">
+          <nav aria-label="artist navigation">
+            <ul class="pagination">
+              <li class="page-item ">
+                <a
+                  class="page-link bg-dark text-white"
+                  href="#"
+                  aria-label="Previous"
+                >
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              {/* <li class="page-item">
+                <a class="page-link" href="#">
+                  1
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  2
+                </a>
+              </li> */}
+              <li class="page-item">
+                <a
+                  class="page-link bg-dark text-white"
+                  href="#"
+                  aria-label="Next"
+                >
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
-    );
+      </div>
+      <div className="row justify-content-evenly row-cols-1 row-cols-md-2 row-cols-lg-4">
+        <div className="col">
+          <Artist />
+        </div>
+        <div className="col">
+          <Artist />
+        </div>
+        <div className="col">
+          <Artist />
+        </div>
+        <div className="col">
+          <Artist />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ArtistList;
-  
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -2,7 +2,37 @@ import Review from "./Review";
 
 function ReviewList() {
   return (
-    <div className="review-list container">
+    <div className="review-list  container">
+      <div className="row header">
+        <h2 className="col-3 text-center">Reviews</h2>
+        {/* {dfkjas;lf} */}
+        <div className="col-3 text-center offset-md-6">
+          <nav aria-label="review navigation">
+            <ul class="pagination">
+              <li class="page-item ">
+                <a class="page-link bg-dark text-white" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              {/* <li class="page-item">
+                <a class="page-link" href="#">
+                  1
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  2
+                </a>
+              </li> */}
+              <li class="page-item">
+                <a class="page-link bg-dark text-white" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
       <div className="row justify-content-evenly row-cols-1 row-cols-md-2 row-cols-lg-4">
         <div className="col">
           <Review />
