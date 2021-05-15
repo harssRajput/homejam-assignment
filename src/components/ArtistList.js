b5fe7ffd1483e7bf64fcc75053c79924c221d6bb
Harsh Rajput <hrrajput20@gmail.com>
2021-05-15 18:37:48 +0530

    add showMore feature for artists

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -9,61 +9,44 @@ class ArtistList extends React.Component {
 
     this.state = {
       artists: artistsDataset,
+      showMoreArtist: false,
     };
   }
 
+  handleShowMoreFeature = () => {
+    console.log("inside show more feature handler");
+
+    this.setState({
+      showMoreArtist: !this.state.showMoreArtist,
+    });
+  };
+
   render() {
     //   console.log(artistsDataset);
-    const { artists } = this.state;
+    const { artists, showMoreArtist } = this.state;
     return (
       <div className="artist-list container">
         <div className="row header">
           <h2 className="col-5 text-center">Upcoming Shows</h2>
           <div className="col-3 text-center offset-md-4">
-            <nav aria-label="artist navigation">
-              <ul className="pagination">
-                <li className="page-item ">
-                  <a
-                    className="page-link bg-dark text-white"
-                    href="#"
-                    aria-label="Previous"
-                  >
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a
-                    className="page-link bg-dark text-white"
-                    href="#"
-                    aria-label="Next"
-                  >
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
+            <p
+              className="show-more-artist"
+              onClick={this.handleShowMoreFeature}
+            >
+              {showMoreArtist ? "- Less" : "+ More"}
+            </p>
           </div>
         </div>
         <div className="row justify-content-evenly row-cols-1 row-cols-md-2 row-cols-lg-4">
-          {artists.map((artist, index) => {
-            return (
-              <div className="col" key={index}>
-                <Artist artist={artist} />
-              </div>
-            );
-          })}
-          {/* <div className="col">
-            <Artist />
-          </div>
-          <div className="col">
-            <Artist />
-          </div>
-          <div className="col">
-            <Artist />
-          </div>
-          <div className="col">
-            <Artist /> 
-        </div>*/}
+          {artists
+            .slice(0, showMoreArtist ? artists.length : 3)
+            .map((artist, index) => {
+              return (
+                <div className="col" key={index}>
+                  <Artist artist={artist} />
+                </div>
+              );
+            })}
         </div>
       </div>
     );
