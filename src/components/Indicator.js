89ae5ebcab5b8e58799b5e7a3719491ad0b9850b
Harsh Rajput <hrrajput20@gmail.com>
2021-05-17 13:16:22 +0530

    add hor scroll to indicators

--- src/components/Indicator.js
+++ src/components/Indicator.js
@@ -2,7 +2,7 @@ import icon from "../icons/microphone.png";
 
 function Indicator() {
   return (
-    <div className="indicator mx-auto my-2 d-flex justify-content-center align-items-center">
+    <div className="indicator">
       <img className="icon" src={icon} alt="icon" />
     </div>
   );
--- src/components/Indicators.js
+++ src/components/Indicators.js
@@ -3,17 +3,18 @@ import Indicator from './Indicator'
 function Indicators() {
     return (
         <div className="indicators container my-3">
-            <div className="row justify-content-evenly row-cols-1 row-cols-md-2 row-cols-lg-4">
-                <div className="col">
+            {/* <div className="row justify-content-evenly row-cols-1 row-cols-md-2 row-cols-lg-4"> */}
+            <div className="hor-scroll">
+                <div className="style-card">
                     <Indicator/>
                 </div>
-                <div className="col">
+                <div className="style-card">
                     <Indicator/>
                 </div>
-                <div className="col">
+                <div className="style-card">
                     <Indicator/>
                 </div>
-                <div className="col">
+                <div className="style-card">
                     <Indicator/>
                 </div>
             </div>
